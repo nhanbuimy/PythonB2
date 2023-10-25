6c5b74e18f3340cb145c39b3df598a82f2dbec28
tupham2832003@gmail.com <tupham2832003@gmail.com>
2023-10-25 17:06:14 +0700

    create project

---
+++ app/dao.py
@@ -0,0 +1,43 @@
+def get_categories():
+    return [{
+        "id": 1,
+        "Name": "SamSung"
+    },
+        {
+            "id": 2,
+            "Name": "Tablet"
+        }
+    ]
+def get_products():
+    products = [{
+        "id": 1,
+        "name": "iPhone 13",
+        "price": 20000000,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-12.png"
+    },
+    {
+        "id": 2,
+        "name": "iPhone 16",
+        "price": 2005555500,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-12.png"
+    },
+    {
+        "id": 3,
+        "name": "iPhone 14",
+        "price": 2000330,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-12.png"
+    },
+    {
+        "id": 4,
+        "name": "iPhone 16",
+        "price": 2005555500,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-12.png"
+    },
+    {
+        "id": 5,
+        "name": "iPhone 14",
+        "price": 2000330,
+        "image": "https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-12.png"
+    }
+    ]
+    return products
---
+++ app/index.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import dao
+
+app = Flask(__name__)
+@app.route("/" )
+def index():
+    cates = dao.get_categories()
+    prods = dao.get_products()
+    return render_template('index.html', categories=cates, products=prods)
+if __name__ == '__main__':
+    app.run(debug=True)
